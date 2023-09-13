9ce9b34eebd85fe981aa89e33d09f126c86dd6a6
Gabe Nunez <gabe@gabenunez.com>
2023-09-13 13:47:10 -0700

    Add matcher

--- middleware.ts
+++ middleware.ts
@@ -8,3 +8,16 @@ export function middleware(request: NextRequest) {
     return NextResponse.redirect(new URL("/500", request.url));
   }
 }
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    "/((?!api|_next/static|_next/image|favicon.ico).*)",
+  ],
+};
