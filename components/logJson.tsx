5391c03acbfe65cd395e66b86e79cae0d224e23b
Gabe Nunez <gabe@gabenunez.com>
2023-09-18 14:18:34 -0700

    Bam

--- components/logJson.tsx
+++ components/logJson.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-export default function LogJson({ json }) {
+export default function LogJson({ json }: { json: string }) {
   console.log(json);
   console.log(JSON.parse(json));
+  return <p>{json}</p>;
 }
