9097ae74a7a37842e1d256787f38e80e78b4487c
Gabe Nunez <gabe@gabenunez.com>
2023-10-06 15:07:19 -0500

    Test repo

--- components/button.tsx
+++ components/button.tsx
@@ -1,11 +1,24 @@
 "use client";
 
 import { setCookie } from "cookies-next";
+import { useState } from "react";
 
 export default function Button() {
+  const [isPopoverOpen, setIsPopoverOpen] = useState(false);
+  const setTheme = (theme: string) => setCookie("theme", theme);
+
+  function handleClick() {
+    setTheme("dark mode of course");
+    setIsPopoverOpen(true);
+
+    if (location) {
+      location.reload();
+    }
+  }
+
   return (
-    <button onClick={() => setCookie("theme", "dark mode of course")}>
-      Set cookie
+    <button onClick={handleClick}>
+      Set cookie {isPopoverOpen ? "Open" : "Closed"}
     </button>
   );
 }
