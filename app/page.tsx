f0dcd4134c37a12a3a40707cd386558acaef26ca
Gabe Nunez <gabe@gabenunez.com>
2024-04-29 10:16:21 -0700

    test

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,9 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Button />
+      <h1>ENV1: {process.env.env1 || "none"}</h1>
+      <h1>ENV2: {process.env.env2 || "none"}</h1>
+      <h1>ENV3: {process.env.env3 || "none"}</h1>
     </main>
   );
 }
